2e7863d7f20b65e2957c024f434096ce9aa41eda
LuanViana21P <luan.leite@sptech.school>
2024-03-16 18:17:20 -0300

    Tela de login no kotlin

---
+++ LoginBuyu/app/src/main/java/com/example/loginbuyu/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.loginbuyu
+
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginBuyu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.loginbuyu)
+    }
+}
