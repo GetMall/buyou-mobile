95592605ce86f77b3597d7aee3f46e39ad7e71fe
FerArj <fernando.araujo@sptech.school>
2024-04-21 20:49:56 -0300

    alterando baseurl

--- BuyuMobile/app/src/main/java/com/example/buyumobile/network/RetrofitService.kt
+++ BuyuMobile/app/src/main/java/com/example/buyumobile/network/RetrofitService.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitService {
-    const val BASE_URL = "http://localhost:8080"
+    const val BASE_URL = "http://localhost:8080/api"
 
     fun getApiUsuarios(): ApiUsuarios {
         val cliente = Retrofit.Builder()
