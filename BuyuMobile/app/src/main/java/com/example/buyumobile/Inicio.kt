654bff0d82429e3f69b67da206d858e33d512cc1
FerArj <fernando.araujo@sptech.school>
2024-04-25 22:11:55 -0300

    refazendo tela de inicio

--- BuyuMobile/app/src/main/java/com/example/buyumobile/Inicio.kt
+++ BuyuMobile/app/src/main/java/com/example/buyumobile/Inicio.kt
@@ -17,9 +17,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -39,12 +43,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.buyumobile.model.ListaShopping
-import com.example.buyumobile.network.RetrofitService
 import com.example.buyumobile.ui.theme.BuyuMobileTheme
-import retrofit2.Callback
-import retrofit2.Call
-import retrofit2.Response
 
 class Inicio : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -64,56 +63,49 @@ class Inicio : ComponentActivity() {
 
     @Composable
     fun TelaInicio(name: String, modifier: Modifier = Modifier) {
-
-        val errorApi = remember {
-            mutableStateOf("")
-        }
-
-        val shoppings = remember {
-            mutableStateOf<List<ListaShopping>>(emptyList())
-        }
-
-        val apiShoppings = RetrofitService.getApiShoppings()
-
-        val get = apiShoppings.getShoppings()
-
-        get.enqueue(object : Callback<List<ListaShopping>> {
-            override fun onResponse(call: Call<List<ListaShopping>>, response: Response<List<ListaShopping>>) {
-                if (response.isSuccessful) {
-                    val shoppingList = response.body()
-                    shoppingList?.let {
-                        shoppings.value = it
-                        println(shoppings.value)
-                    }
-                }
-            }
-
-            override fun onFailure(call: Call<List<ListaShopping>>, t: Throwable) {
-                errorApi.value = t.message.toString()
-            }
-        })
-
         Column {
             Header()
-            ShoppingsProximos()
-            shoppings.value.forEach { shopping ->
-                ListaShoppings(nomeShopping = shopping.nome, logoShopping = painterResource(R.drawable.eldorado_logo))
-                ListaShoppings(nomeShopping = "Teste", logoShopping = painterResource(R.drawable.eldorado_logo))
-            }
+            Text(
+                text = "Shoppings próximos a você",
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(start = 36.dp, top = 60.dp, bottom = 25.dp)
+                    .fillMaxWidth(),
+                color = Color.Black
+            )
+            ShoppingsProximos(nomeShoppingProximo = "Shopping", logoShoppingProximo = painterResource(
+                id = R.drawable.eldorado_logo
+            ))
+            Text(
+                text = "Shoppings populares",
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(start = 36.dp, top = 60.dp, bottom = 25.dp)
+                    .fillMaxWidth(),
+                color = Color.Black
+            )
+            ListaShoppings(nomeShopping = "Shopping Cidade de São Paulo", logoShopping = painterResource(id = R.drawable.cidade_sp_log))
+            ListaShoppings(nomeShopping = "Shopping Pátio Paulista", logoShopping = painterResource(id = R.drawable.patio_paulista_logo))
+            ListaShoppings(nomeShopping = "Shopping Eldorado", logoShopping = painterResource(id = R.drawable.eldorado_logo))
+            ListaShoppings(nomeShopping = "Shopping Eldorado", logoShopping = painterResource(id = R.drawable.eldorado_logo))
+            ListaShoppings(nomeShopping = "Shopping Eldorado", logoShopping = painterResource(id = R.drawable.eldorado_logo))
+            ListaShoppings(nomeShopping = "Shopping Eldorado", logoShopping = painterResource(id = R.drawable.eldorado_logo))
+            ListaShoppings(nomeShopping = "Shopping Eldorado", logoShopping = painterResource(id = R.drawable.eldorado_logo))
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun Header() {
-
         val (endereco, setEndereco) = remember {
             mutableStateOf("")
         }
-
         Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(200.dp)
+                .height(100.dp)
         ) {
             Image(
                 painter = painterResource(id = R.drawable.fundo_roxo),
@@ -123,100 +115,98 @@ class Inicio : ComponentActivity() {
                 contentScale = ContentScale.Crop
             )
 
-            Column(
+            OutlinedTextField(
+                value = endereco,
+                onValueChange = setEndereco,
+                label = { Text("Digite seu CEP", style = MaterialTheme.typography.bodyMedium) },
                 modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                verticalArrangement = Arrangement.Top,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.logotipo_branco_buyu),
-                        contentDescription = "Logo da Empresa",
-                        modifier = Modifier.size(48.dp)
-                    )
-
-                    Image(
-                        painter = painterResource(id = R.drawable.boneco_carrinho),
-                        contentDescription = "Logo da Empresa",
-                        modifier = Modifier.size(48.dp)
-                    )
-                }
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.fillMaxWidth()
-                )
-                {
-                Box(
-                    modifier = Modifier
-                        .weight(1f)
-                        .height(55.dp)
-                        .clip(RoundedCornerShape(10.dp)),
-                    contentAlignment = Alignment.Center
-                )
-                    {
-                        Row(verticalAlignment = Alignment.CenterVertically) {
-                            Spacer(modifier = Modifier.width(16.dp))
-                            TextField(value = endereco, onValueChange = setEndereco,
-                                modifier = Modifier
-                                    .height(70.dp),
-                                placeholder = {Text("Digite o CEP")},
-                                colors = TextFieldDefaults.colors(
-                                    focusedIndicatorColor = Color.White,
-                                    unfocusedIndicatorColor = Color.White,
-                                    unfocusedContainerColor = Color.White,
-                                    focusedContainerColor = Color.White
-                                ),
-                            )
-
-                            Spacer(modifier = Modifier.weight(1f))
-
-                            Box(
-                                modifier = Modifier
-                                    .size(55.dp)
-                                    .clip(RoundedCornerShape(10.dp))
-                                    .background(Color.Black),
-                                contentAlignment = Alignment.Center
-                            ) {
-                                Image(
-                                    painter = painterResource(id = R.drawable.lupa),
-                                    contentDescription = "Logo da Empresa",
-                                    modifier = Modifier.size(28.dp)
-                                )
-                            }
-                        }
-                    }
-                    Spacer(modifier = Modifier.width(8.dp))
-                }
-            }
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+                    .background(Color.Transparent, RoundedCornerShape(8.dp))
+                    .clip(RoundedCornerShape(8.dp))
+                    .align(Alignment.CenterStart)
+                    .height(60.dp),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = Color.White,
+                unfocusedBorderColor = Color.White,
+                    focusedLabelColor = Color.White,
+                    unfocusedLabelColor = Color.White
+            ),
+            )
         }
     }
 
     @Composable
-    fun ShoppingsProximos() {
-        Text(
-            text = "Shoppings próximos a você",
-            fontSize = 15.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier
-                .padding(start = 36.dp, top = 60.dp, bottom = 25.dp)
-                .fillMaxWidth(),
-            color = Color.Black
-        )
+    fun ShoppingsProximos(nomeShoppingProximo: String, logoShoppingProximo: Painter) {
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+
+                Spacer(modifier = Modifier.width(18.dp))
+
+                Image(
+                    painter = logoShoppingProximo,
+                    contentDescription = "Logo do Shopping",
+                    modifier = Modifier.size(35.dp)
+                )
+
+                Spacer(modifier = Modifier.width(0.dp))
+
+                Image(
+                    painter = logoShoppingProximo,
+                    contentDescription = "Shopping Logo",
+                    modifier = Modifier
+                        .size(24.dp)
+                        .padding(start = 8.dp, end = 16.dp)
+                )
+
+                Text(
+                    text = nomeShoppingProximo,
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Normal
+                )
+            }
+
+            Spacer(modifier = Modifier.height(15.dp))
+
+            LazyRow (
+                horizontalArrangement = Arrangement.Center
+            ){
+                items(2) {
+                    Column (
+                        modifier = Modifier
+                            .padding(start = 4.dp)
+                            .width(100.dp)
+                    ){
+                        Image(
+                            painter = painterResource(id = R.drawable.logo_renner),
+                            contentDescription = "Logo da Empresa",
+                            modifier = Modifier
+                                .size(50.dp)
+                                .clip(RoundedCornerShape(10.dp))
+                                .background(Color.White)
+                        )
+                        Text(
+                            text = "Renner",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 10.sp,
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+        }
     }
 
     @Composable
     fun ListaShoppings(nomeShopping: String, logoShopping: Painter) {
         Spacer(modifier = Modifier.height(16.dp))
-
         Column(
             modifier = Modifier.fillMaxWidth()
         ) {
@@ -252,29 +242,31 @@ class Inicio : ComponentActivity() {
 
             Spacer(modifier = Modifier.height(15.dp))
 
-            Row(
-                modifier = Modifier.horizontalScroll(rememberScrollState())
-            ) {
-                ListaLojas(
-                    nomeShopping = "",
-                    nomeLoja = "Riachuelo",
-                    logoLoja = painterResource(id = R.drawable.logo_riachuelo)
-                )
-                ListaLojas(
-                    nomeShopping = "",
-                    nomeLoja = "  Renner",
-                    logoLoja = painterResource(id = R.drawable.logo_renner)
-                )
-                ListaLojas(
-                    nomeShopping = "",
-                    nomeLoja = " Rihappy",
-                    logoLoja = painterResource(id = R.drawable.logo_rihappy)
-                )
-                ListaLojas(
-                    nomeShopping = "",
-                    nomeLoja = "    C&A",
-                    logoLoja = painterResource(id = R.drawable.logo_cea)
-                )
+            LazyRow (
+                horizontalArrangement = Arrangement.Center
+            ){
+                items(5) {
+                    Column (
+                        modifier = Modifier
+                            .padding(start = 4.dp)
+                            .width(100.dp)
+                    ){
+                        Image(
+                            painter = painterResource(id = R.drawable.logo_renner),
+                            contentDescription = "Logo da Empresa",
+                            modifier = Modifier
+                                .size(50.dp)
+                                .clip(RoundedCornerShape(10.dp))
+                                .background(Color.White)
+                        )
+                        Text(
+                            text = "Renner",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 10.sp,
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                }
             }
 
             Spacer(modifier = Modifier.height(16.dp))
